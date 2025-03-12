941de44e9bb24a013ff29c3c1c6bf4dbcd39df17
Dmoore8122 <173196877+Dmoore8122@users.noreply.github.com>
2025-03-12 14:20:03 -0400

    I created the operate function the take in arguments and put them through the function that corresponds with the operator.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-
+const value1 = '';
+const value2 = '';
+const operator = '';
 
 const add = (a, b) => {
     return a + b;
@@ -27,9 +29,21 @@ const multiply = (a, b) => {
 const divide = (a, b) => {
     return a / b;
 };
-const divideArray = (arr) => {
+/*const divideArray = (arr) => {
     let value = arr.reduce((sum, current) => sum / current, );
     return value;
-}
+};*/
 
-console.log(divideArray([225, 25, 6]))
+const operate = (num1, op, num2) => {
+    if (op === '+') {
+        return add(num1, num2);
+    } else if (op === '-') {
+        return subtract(num1, num2);
+    } else if (op ==='*') {
+        return multiply(num1, num2);
+    }else if (op === '/') {
+        return divide(num1, num2);
+    };
+};
+
+console.log(operate(12, '/', 4))
